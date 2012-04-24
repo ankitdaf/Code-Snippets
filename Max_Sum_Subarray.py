4db04a8823b2ad0e9cb7a256a991736706acf810
Ankit Daftery <ankitdaf@gmail.com>
2012-04-24 21:45:56 +0530

    Fixed test cases where the algorithm breaks

--- Max_Sum_Subarray.py
+++ Max_Sum_Subarray.py
@@ -3,20 +3,46 @@ def max_sum_subarray(A):
     Given an array of positive and negative integers,find the maximum sum contiguous subarray.
     Takes list as input.
     """
-    max_sum = 0
-    current_sum = 0
-    start=0
-    end=0
-    for i in range(0,len(A)):
+    max_sum = A[0]
+    current_sum = max_sum
+    start=end=sp=ep=0
+    length=templen=1
+    for i in range(1,len(A)):
         if(A[i] > (current_sum + A[i])):
-            current_sum = A[i]
-            start = i
-            end = i
+            if(A[i] > current_sum):
+                current_sum = A[i]
+                sp=i
+                ep=i
+                if (current_sum >= max_sum):
+                    max_sum = current_sum
+                    if ((ep-sp+1) >= length):
+                        start = sp
+                        end = ep
+                        length = ep-sp+1
+            else:
+                sp=i
+                ep=i
+            current_sum = A[i]              
         else:
-            current_sum += A[i]
-        if (current_sum > max_sum):
-            max_sum = current_sum
-            end = i
+            if(A[i]>=0):
+                current_sum += A[i]
+                ep = i
+                if (current_sum >= max_sum):
+                    max_sum = current_sum
+                    if ((ep-sp+1) > length):
+                        start = sp
+                        end = ep
+                        length = ep-sp+1
+            else:
+                if (current_sum >= max_sum):
+                    max_sum = current_sum
+                    if ((ep-sp+1) > length):
+                        start = sp
+                        end = ep
+                        length = ep-sp+1
+                        sp=i
+                        ep=i
+                current_sum = A[i]
     return start,end,max_sum
 
 A = input("Please enter the input array\n")
