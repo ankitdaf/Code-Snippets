512b383035d00d1c70476499476e4262a77e9c9f
Ankit Daftery <ankitdaf@gmail.com>
2014-08-06 00:27:41 +0530

    Backup software sources too

--- Package-Replication/README.md
+++ Package-Replication/README.md
@@ -1,6 +1,6 @@
 Package-Replication
 ====================
 
-This repo is meant to backup and restore my installed packages, so I can clone my machines quickly. 
+This repo is meant to backup and restore my installed packages and software sources, so I can clone my machines quickly. 
 
 Hopefully it will grow to include more stuff.
--- Package-Replication/backuprestore-package.sh
+++ Package-Replication/backuprestore-package.sh
@@ -5,6 +5,8 @@
 
 # Backup my packages
 sudo dpkg --get_selections  | grep -v deinstall > selections.list
+# Backup my sources list
+sudo cp /etc/apt/sources.list ./
 
 #Restore my packages
 sudo dpkg --clear-selections
@@ -12,3 +14,8 @@ sudo dpkg --set-selections < selections.list
 sudo apt-get install dselect
 sudo dselect update
 sudo dselect install
+
+#Restore my software sources, force overwrite but make a backup
+#To append or to replace, that is the question
+
+sudo cp -fb ./sources.list /etc/apt/
